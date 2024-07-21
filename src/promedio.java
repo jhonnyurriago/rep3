acbb64e2bc0915922d0e96d39ac756d0e84bb242
jhonnyUrriago <jhonnyurriago997@gmail.com>
2024-07-21 17:44:48 -0300

    aqui redapto algunos tipos de bariables. tambien especifico para que sirven

---
+++ src/promedio.java
@@ -0,0 +1,22 @@
+public class promedio {
+public static void main(String[] args) {
+
+    //esta es una bariable tipo String; com mayuscula. se usa para representar texto
+//String nombre= "jhonny";
+//String saludo="hola, mi nombre es ";
+//System.out.println(saludo+nombre);
+
+    /* esta es una bariable de tipo int;numero entero, tambien podria ser representada con numemeros decimales con double
+int numero= 10;
+numero = numero + 10;
+System.out.println(numero );
+
+     */
+    /* aqui reprersento una bariable booleana; solo dan como resultado false o true
+    boolean mayorDeEdad = ;
+    System.out.println(mayorDeEdad);
+    */
+
+}  
+}
+
